e6c51d3abcf8761a9075c26e2f004392ea87df31
Pinal Naik <pinal.naik@gmail.com>
2022-08-18 21:42:45 +0530

    First commit from Xcode

--- XcodeGitHubDemo/ContentView.swift
+++ XcodeGitHubDemo/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("This is my first commit from Xcode!")
             .padding()
     }
 }
