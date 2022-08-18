f2e651588db3c30923911a951f1865e6f6315b70
Pinal Naik <pinal.naik@gmail.com>
2022-08-18 21:35:15 +0530

    Initial Commit

---
+++ XcodeGitHubDemo/XcodeGitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGitHubDemoApp.swift
+//  XcodeGitHubDemo
+//
+//  Created by Pinal Naik on 18/08/22.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
